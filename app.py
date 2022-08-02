a1426bee65f0b121f79d7830d17399f71303c1ad
CheongYa <chch1234569@naver.com>
2022-08-02 16:12:27 +0900

    chpater3(1)

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from gettext import install
 from flask import Flask
+from controllers.product import product
 
 app = Flask(__name__)
 
-@app.route("/")
+app.register_blueprint(product, url_prefix='/products')
+
+@app.route("/", methods=['GET']) # methods=['GET'] 방식일 때는 생략이 가능(기본값)
 def hello():
     return "Hello, World!"
+
+if __name__ == "__main__": # 자동으로 업데이트가 되지만 flask run이 아닌 python app.py를 이용하여 시작
+    app.run(debug=True)
---
+++ controllers/product.py
@@ -0,0 +1,37 @@
+from flask import request, render_template
+from .blueprint import product
+from models.product import Product
+from datetime import datetime
+from werkzeug.utils import secure_filename
+import os
+
+
+@product.route('/form')
+def form():
+    return render_template('product_form.html')
+
+
+# 상품 등록 API
+@product.route('/regist', methods=['POST']) # blueprint.py 쪽으로 들어감
+def regist():
+    # 전달받은 상품 정보
+    form_data = request.form
+
+    # 이미지 파일 정보
+    thumbnail_img = request.files.get('thumbnail_img')
+    detail_img = request.files.get('detail_img')
+    thumbnail_img_url = _upload_file(thumbnail_img)
+    detail_img_url = _upload_file(detail_img)
+    Product.insert_one(form_data, thumbnail_img_url, detail_img_url) # 저장하는 일
+
+    return "상품 등록 API입니다."
+
+def _upload_file(img_file):
+    timestamp = str(datetime.now().timestamp())
+    filename = timestamp + '_' + secure_filename(img_file.filename)
+    image_path = f'./static/uploads'
+    os.makedirs(image_path, exist_ok=True)
+    img = os.path.join(image_path, filename)
+    img_file.save(img)
+
+    return f'./static/uploads' + filename
